7d970593df9d2e7cd4d39c8ce58471f0631ce30e
artemburiak <87930900+artemburiak@users.noreply.github.com>
2021-07-25 12:51:29 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,8 @@
+name = "Artem"
+age = 14
+city = "Kyiv"
+print("My name is", name)
+print("I live in", city)
+print("I am", age, "years old")
+
+print("My name is", name + ". I live in", city + ". I am", age, "years old.")
